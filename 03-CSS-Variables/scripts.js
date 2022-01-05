e1b066e822c840c43245f5d0eeb9afd25887a735
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-01-06 00:31:11 +0530

    03-CSS-Variables

---
+++ 03-CSS-Variables/scripts.js
@@ -0,0 +1,15 @@
+// DOM Elements
+const inputs = document.querySelectorAll(".controls input");
+
+// Functions
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    `${this.value}${suffix}`
+  );
+}
+
+// Event Listeners
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
